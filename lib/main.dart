a50d277c2ab3f37739688a72a1ecd3416d75193d
shradha-79 <66915839+shradha-79@users.noreply.github.com>
2021-05-18 00:57:35 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         fontFamily: "Brand Bold",
         primarySwatch: Colors.blue,
       ),
-      initialRoute: LoginScreen .idScreen,
+      initialRoute: MainScreen.idScreen,
       routes:
           {
           RegistrationScreen.idScreen: (context) => RegistrationScreen(),
