1c2f71830317100a2cf85b672b7a5422b8846862
Shivangi <raishivangi1708@gmail.com>
2021-05-18 21:18:50 +0530

    new changes

--- lib/AllScreen/mainscreen.dart
+++ lib/AllScreen/mainscreen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:saans_app/AllWidgets/Divider.dart';
 
@@ -21,6 +22,21 @@ class _MainScreenState extends State<MainScreen> {
   GoogleMapController newGoogleMapController;
 
   GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+  Position currentPosition;
+  var geolocator = Geolocator();
+
+  double bottomPaddingOfMap = 0;
+  void locatePosition()async
+  {
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    currentPosition = position;
+
+    LatLng letLatPosition = LatLng(position.latitude, position.longitude);
+
+    CameraPosition cameraPosition = new CameraPosition(target: letLatPosition, zoom: 14);
+    newGoogleMapController.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+  }
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
@@ -84,13 +100,22 @@ class _MainScreenState extends State<MainScreen> {
       body: Stack(
         children: [
           GoogleMap(
+            padding: EdgeInsets.only(bottom: bottomPaddingOfMap),
             mapType: MapType.normal, 
             myLocationButtonEnabled: true, 
-            initialCameraPosition: _kGooglePlex, 
+            initialCameraPosition: _kGooglePlex,
+            myLocationEnabled:  true,
+            zoomGesturesEnabled: true,
+            zoomControlsEnabled: true,
             onMapCreated: (GoogleMapController controller)
             {
               _controllerGoogleMap.complete(controller);
               newGoogleMapController= controller;
+
+              setState(() {
+                bottomPaddingOfMap = 300.0;
+              });
+              locatePosition();
             }, 
           ), //MAP
 
@@ -133,7 +158,7 @@ class _MainScreenState extends State<MainScreen> {
             right:0.0,
             bottom:0.0,
             child:Container(
-              height:320.0,
+              height:300.0,
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(topLeft: Radius.circular(15.0),topRight:Radius.circular(15.0)),
