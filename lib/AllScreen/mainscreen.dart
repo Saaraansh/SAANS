ab0a1058257fddb57b8f1da0f029ce186f7339f5
HarshvardhanJoshi <Harshitjoshi001@gmail.com>
2021-05-18 11:27:00 +0530

    Navigation Menu

--- lib/AllScreen/mainscreen.dart
+++ lib/AllScreen/mainscreen.dart
@@ -16,6 +16,8 @@ class _MainScreenState extends State<MainScreen> {
   Completer<GoogleMapController> _controllerGoogleMap = Completer();
   GoogleMapController newGoogleMapController;
 
+  GlobalKey<ScaffoldState> scaffoldkey = new GlobalKey<ScaffoldState>();
+
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
@@ -23,9 +25,57 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldkey,
       appBar: AppBar(
         title: Text("Main Screen"),
-      ), 
+      ), //APPBAR
+      drawer: Container(
+        color: Colors.white,
+        width:255.0,
+        child: Drawer(
+          child: ListView(
+            children: [
+              Container(
+                height:165.0,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: Row(
+                    children:[
+                      Image.assest("images/user_icon.png", height: 65.0, width:65.0,),
+                      SizedBox(width:16.0,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children:[
+                          Text("Profile Name", style:TextStyle(fontSize:16.0, fontFamily:"Brand-Bold"),),
+                          SizedBox(height:6.0,),
+                          Text("Visit Profile"),
+
+                        ],
+                      ),
+
+                    ],
+                  ),
+                ),
+              ), //INNERCONTAINER
+
+              DividerWidget(),
+              SizeBoxed(height:12.0,),
+              ListTile(
+                leading:Icon(Icons.history),
+                title: Text("History", style: TextStyle(fontSize:15.0,),),
+              ),
+              ListTile(
+                leading:Icon(Icons.person),
+                title: Text("Visit Profile", style: TextStyle(fontSize:15.0,),),
+              ),
+              ListTile(
+                leading:Icon(Icons.info),
+                title: Text("About", style: TextStyle(fontSize:15.0,),),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Stack(
         children: [
           GoogleMap(
@@ -37,7 +87,132 @@ class _MainScreenState extends State<MainScreen> {
               _controllerGoogleMap.complete(controller);
               newGoogleMapController= controller;
             }, 
-          ), 
+          ), //MAP
+
+        // HAMBURGER BTN
+        Positioned(
+          top:45.0,
+          left:22.0,
+          child:GestureDetector(
+            onTap:()
+            {
+              scaffoldkey.currentState.openDrawer(),
+            },
+          child: Container(
+          decoration: BoxDecoration(
+            color:Colors.white,
+            border.Radius:BorderRadius.circular(22.0),
+            boxShadow:[
+              BoxShadow(
+                color:Colors.black,
+                blurRadius:6.0,
+                spreadRadius: 0.5,
+                offset: Offset(
+                  0.7,
+                  0.7,
+                ),
+              ),
+            ],
+          ), //BOXDECORATION
+          child: CircleAvatar(
+            backgroundColor: Colors.white,
+            child: Icon(Icons.menu, color:Colors.black,),
+            radius:20.0,
+          ),
+        ),
+        ),
+      ),
+
+          Positioned(
+            left:0.0,
+            right:0.0,
+            bottom:0.0,
+            child:Container(
+              height:320.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(15.0),topRight:Radius.circular(15.0)),
+                boxShadow:[
+                  BoxShadow(
+                    color:Colors.black,
+                    blurRadius: 16.0,
+                    spreadRadius: 0.5,
+                    offset: Offset(0.7,0.7),
+                  ),//BOXSHAD
+                ],
+              ),//BOXDEC
+              child Padding(
+                padding: const EdgeInsets.symmetric(horizontal:24.0, vertical:18.0),
+                child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start, 
+                children:[
+                  SizedBox(height: 6.0),
+                  Text("Hello", style: TextStyle(fontSize:10.0),),
+                  Text("Where to?", style: TextStyle(fontSize:20.0, fontFamily:"Brand-Bold"),),
+                  SizedBox(height: 20.0),
+                  Container(
+                     decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5.0),
+                boxShadow:[
+                  BoxShadow(
+                    color:Colors.black54,
+                    blurRadius: 6.0,
+                    spreadRadius: 0.5,
+                    offset: Offset(0.7,0.7),
+                  ),//BOXSHAD2
+                ],
+              ),//BOXDEC2
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                children:[
+                  Icon(Icons.search, color: Colors.greenAccent,),
+                  SizedBox(width:10.0,),
+                  Text("Seach Drop off")
+                ],
+              ),
+              ),
+                  ),//Container2
+                  SizedBox(height: 24.0),
+                  Row(
+                    children:[
+                      Icon(Icons.home, color: Colors.grey,),
+                      SizedBox(width:12.0,)
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children:[
+                          Text("Add Home"),
+                          SizedBox(height:4.0,),
+                          Text("Your Home Address", style: TextStyle(color: Colors.black54, fontSize:12.0),),
+                        ],
+                      ),
+                    ],
+                  ), //ROW
+
+                   SizedBox(height: 10.0),
+                  DividerWidget(
+                   SizedBox(height: 16.0),  
+                  ),
+                  Row(
+                    children:[
+                      Icon(Icons.work, color: Colors.grey,),
+                      SizedBox(width:12.0,)
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children:[
+                          Text("Add Work"),
+                          SizedBox(height:4.0,),
+                          Text("Your Work Address", style: TextStyle(color: Colors.black54, fontSize:12.0),),
+                        ],
+                      ),
+                    ],
+                  ), //ROW2
+                ],
+              ),
+              ),
+            ), //Container
+          ),
         ], 
       ), 
     );
