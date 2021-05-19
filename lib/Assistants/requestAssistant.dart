25c867ef118403dca3e36ccdd357e4add1fdf548
Saaransh Panjrath <saaranshpanjrath@outlook.com>
2021-05-19 17:52:13 +0530

    done

--- lib/Assistants/requestAssistant.dart
+++ lib/Assistants/requestAssistant.dart
@@ -1,28 +1,20 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class RequestAssistant
-{
-  static Future<dynamic> getRequest(String url) async
-  {
-    http.Response response = await http.get(url);
-    try
-    {
-      if(response.statusCode == 200)
-      {
+class RequestAssistant {
+  // ignore: non_constant_identifier_names
+  static Future<dynamic> getRequest(String url) async {
+    http.Response response = await http.get(Uri.parse(url));
+    try {
+      if (response.statusCode == 200) {
         String jSonData = response.body;
         var decodeData = jsonDecode(jSonData);
         return decodeData;
-      }
-      else
-      {
+      } else {
         return "failed";
       }
-    }
-    catch(exp)
-    {
+    } catch (exp) {
       return "failed";
-    }    
+    }
   }
-  
 }
