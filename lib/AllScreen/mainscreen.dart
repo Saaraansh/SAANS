ffef23fba07ab33429fb3b9c5fa9a8b93de39c6c
shradha <shradha-79>
2021-05-21 11:36:02 +0530

    updated

--- lib/AllScreen/mainscreen.dart
+++ lib/AllScreen/mainscreen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
@@ -173,7 +174,7 @@ class _MainScreenState extends State<MainScreen> {
             right:0.0,
             bottom:0.0,
             child:Container(
-              height:300.0,
+              height:200.0,
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(topLeft: Radius.circular(15.0),topRight:Radius.circular(15.0)),
@@ -273,6 +274,94 @@ class _MainScreenState extends State<MainScreen> {
               ),
             ), //Container
           ),
+          
+          Positioned(
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              height: 300.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius:BorderRadius.only(topLeft: Radius.circular(16.0), topRight: Radius.circular(16.0),),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    blurRadius: 0.5,
+                    offset: Offset(0.7, 0.7),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 17.0),
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      color: Colors.tealAccent[100],
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Row(
+                          children: [
+                            Image.asset("images/ambulance.png", height: 70.0, width: 80.0,),
+                            SizedBox(width: 16.0,),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Ambulance",style: TextStyle(fontSize: 18.0, fontFamily: "Brand-Bold",)
+                                ),
+                                Text(
+                                  "10Km",style: TextStyle(fontSize: 18.0, color: Colors.grey,),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 20.0,),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        children: [
+                          Icon(FontAwesomeIcons.moneyCheckAlt, size: 18.0, color: Colors.black54,),
+                          SizedBox(width: 16.0,),
+                          Text("Cash"),
+                          SizedBox(width: 6.0,),
+                          Icon(Icons.keyboard_arrow_down, color: Colors.black54, size: 16.0,),
+                        ],
+                      ),
+                    ),
+
+                    SizedBox(height: 24.0,),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: RaisedButton(
+                        onPressed: ()
+                        {
+                          print("clicked");
+                        },
+                        color: Theme.of(context).accentColor,
+                        child: Padding(
+                          padding: EdgeInsets.all(17.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text("Request", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white),),
+                              Icon(FontAwesomeIcons.taxi, color: Colors.white, size: 26.0,),
+                            ],
+                          ),
+                        )
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            
+            ),
+          )
         ], 
       ), 
     ); //SCAFFOLD
